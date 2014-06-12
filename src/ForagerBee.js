b67a52548be9df93820bfcb3eeb3ec440eb179ef
Austen Talbot <austentalbot@gmail.com>
2014-06-12 10:25:49 -0700

    Completed bees repo.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+  Bee.apply(this, arguments);
+  this.age=10;
+  this.job='find pollen';
+  this.color='yellow';
+  this.canFly=true;
+  this.treasureChest=[];
+
 };
 
+ForagerBee.prototype=Object.create(Bee.prototype);
+ForagerBee.prototype.constructor=ForagerBee;
+
+ForagerBee.prototype.forrage=function(treasure) {
+  this.treasureChest.push(treasure);
+}
